518acdbec73d2a1e2d563428e3a823a923a2e3f7
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2018-09-05 18:22:54 +0000

    Added users get and create

--- src/index.js
+++ src/index.js
@@ -4,23 +4,48 @@ const options = {
   port: 8000
 }
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+//TODO replace with real database
+let users = [{
+  id: "user-1",
+  name: "Bob",
+  todos: [{
+    id: "todo-1",
+    text: "Mop Ceiling"
+  }]
+}]
 
-// 2
+let userIdCount = users.length;
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Todo list application`
+    info: () => `Welcome to the Todo list website`,
+
+    users: () => users,
+  },
+
+  Mutation: {
+    createUser: (root, args) => {
+      const user = {
+        id: `user-${++userIdCount}`,
+        name: args.name,
+        todos: []
+      }
+
+      users.push(user);
+      return user;
+    }
+  },
+
+  User: {
+    id: (root) => root.id,
+    name: (root) => root.name,
+    todos: (root) => root.todos
   }
 }
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(options, ({ port }) => 
