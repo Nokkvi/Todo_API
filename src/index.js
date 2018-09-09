dc76fd89f9ae0b00a88572adc27bc49b5101d8af
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2018-09-09 22:16:22 +0000

    Added database layer

--- src/index.js
+++ src/index.js
@@ -1,77 +1,63 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
 const options = {
   port: 8000
 }
-
-//TODO replace with real database
-let users = [{
-  id: "user-1",
-  name: "Bob",
-  password: "password123",
-  todos: [{
-    id: "todo-1",
-    text: "Mop Ceiling",
-  }]
-}]
-
-function getTodoIdCount() {
-  let count = 0;
-  users.forEach(function(user) {
-    count += user.todos.length;
-    console.log(count);
-  });
-  return count;
-}
-
-let userIdCount = users.length;
-
-let todoIdCount = getTodoIdCount();
 
 
 const resolvers = {
   Query: {
     info: () => `Welcome to the Todo list website`,
 
-    users: () => users,
+    users: (root, args, context, info) => {
+      return context.db.query.users({}, info);
+    },
 
-    user: (root, args) => {
-      for(let i = 0; i < users.length; i++){
-        if(users[i].id === args.id){
-          return users[i];
+    user: (root, args, context, info) => {
+      return context.db.query.user({
+        where: {
+          id: args.id
         }
-      }
+      }, info);
     }
   },
 
   Mutation: {
-    createUser: (root, args) => {
-      const user = {
-        id: `user-${++userIdCount}`,
-        name: args.name,
-        password: args.password,
-        todos: []
-      }
-
-      users.push(user);
-      return user;
-    },
-
-    createTodo: (root, args) => {
-      const todo = {
-        id: `todo-${++todoIdCount}`,
-        text: args.text
-      }
-
-      for(let i = 0; i < users.length; i++) {
-        if(users[i].id === args.userId) {
-          users[i].todos.push(todo);
-          return users[i];
+    createUser: (root, args, context, info) => {
+      return context.db.mutation.createUser({
+        data: {
+          name: args.name,
+          password: args.password
         }
-      }
+      }, info)
     },
 
-    changePassword: (root, args) => {
+    createTodo: (root, args, context, info) => {
+      return context.db.mutation.createTodo({
+        data: {
+          text: args.text,
+          done: false,
+          dueAt: args.dueAt,
+          user: {
+            connect: {
+              id: args.userId
+            }
+          }
+        }
+      }, info)
+    },
+
+    //TODO connect to db
+    changePassword: (root, args, context, info) => {
+      return context.db.mutation.updateUser({
+        data: {
+          password: args.password
+        },
+        where: {
+          id: args.userId 
+        }
+      }, info)
       for(let i = 0; i < users.length; i++) {
         if(users[i].id === args.id) {
           users[i].password = args.password;
@@ -80,14 +66,12 @@ const resolvers = {
       }
     },
 
-    deleteUser: (root, args) => {
-      for(let i = 0; i < users.length; i++) {
-        if(users[i].id === args.id) {
-          users.splice(i, 1);
-          return true;
+    deleteUser: (root, args, context, info) => {
+      return context.db.mutation.deleteUser({
+        where: {
+          id: args.userId
         }
-      }
-      return false;
+      }, info);
     }
   },
 
@@ -102,7 +86,17 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/nokkvi-sverrisson-13c544/database/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 })
+
 server.start(options, ({ port }) => 
   console.log(
     `Server is running on http://localhost:${port}`,
